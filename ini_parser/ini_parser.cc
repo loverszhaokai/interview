72ea2e591e602eb7f65aae2f6332ccc15f8c14a9
Kai Zhao <loverszhao@gmail.com>
2015-08-28 19:23:45 +0800

    Solve ini_parser

--- ini_parser/ini_parser.cc
+++ ini_parser/ini_parser.cc
@@ -1,7 +1,127 @@
 #include "ini_parser.h"
 
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 namespace qh
 {
+INIParser::INIParser()
+{
+	kvalue.clear();
+}
+
+INIParser::~INIParser()
+{
+	kvalue.clear();
+}
+
+bool INIParser::Parse(const char* ini_data, size_t ini_data_len,
+	const std::string& line_seperator, const std::string& key_value_seperator)
+{
+	int index = 0, pre_index = 0;
+	std::string key, value;
+
+	if (NULL == ini_data || ini_data_len == 0 ||
+		line_seperator == key_value_seperator ||
+		key_value_seperator == "")
+		return false;
+
+	char cnt_c;
+	int ls_index = 0, ls_pos;
+	int kvs_index = 0, kvs_pos;
+	int key_pos = 0;
+	bool find_ls = false, find_kvs = false;
+	bool ret = false;
+
+	while (index < ini_data_len) {
+
+		cnt_c = ini_data[index];
+
+		if (!find_kvs)
+		if (key_value_seperator[kvs_index] == cnt_c) {
+			kvs_index++;
+			if (kvs_index == key_value_seperator.size()) {
+				find_kvs = true;
+				kvs_pos = index - kvs_index + 1;
+			}
+		} else {
+			kvs_index = 0;
+		}
+
+		if (!find_ls)
+		if (line_seperator.size() != 0 && line_seperator[ls_index] == cnt_c) {
+			ls_index++;
+			if (ls_index == line_seperator.size()) {
+				find_ls = true;
+				ls_pos = index - ls_index + 1;
+			}
+		} else {
+			ls_index = 0;
+		}
+
+		if (find_ls && key_pos == ls_index) {
+
+			key_pos = index + 1;
+			ls_index = 0;
+			find_ls = false;
+			kvs_index = 0;
+			find_kvs = false;
+
+		} else if (find_ls && find_kvs) {
+
+			key = std::string(ini_data + key_pos, kvs_pos - key_pos);
+			value = std::string(ini_data + kvs_pos + key_value_seperator.size(),
+				ls_pos - key_pos - key_value_seperator.size() - 1);
+
+			kvalue[key] = value;
+			ret = true;
+
+			key_pos = index + 1;
+			ls_index = 0;
+			find_ls = false;
+			kvs_index = 0;
+			find_kvs = false;
+
+		} else if (find_ls && !find_kvs) {
+
+			key_pos = index + 1;
+			ls_index = 0;
+			find_ls = false;
+			kvs_index = 0;
+			find_kvs = false;
+
+		}
+
+		index++;
+	}
+
+	if (find_kvs) {
+			key = std::string(ini_data + key_pos, kvs_pos - key_pos);
+			value = std::string(ini_data + kvs_pos + key_value_seperator.size(),
+				ini_data_len - key_pos - key_value_seperator.size() - 1);
+
+			kvalue[key] = value;
+			ret = true;
+	}
+
+	return ret;
+}
+
+const std::string& INIParser::Get(const std::string& key, bool* found)
+{
+	if (kvalue.find(key) != kvalue.end()) {
+		if (found)
+			*found = true;
+		return kvalue[key];
+	}
+
+	if (found)
+		*found = false;
+
+	static std::string str = "";
+	return str;
+}
 
 }
--- ini_parser/main.cc
+++ ini_parser/main.cc
@@ -3,6 +3,11 @@
 #include <string.h>
 #include <assert.h>
 
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
 void test1()
 {
     const char* ini_text= "a=1\nb=2\n"; 
@@ -14,10 +19,11 @@ void test1()
     const std::string& a = parser.Get("a", NULL);
     assert(a == "1");
 
-    std::string b = parser.Get("a", NULL);
+    std::string b = parser.Get("b", NULL);
     assert(b == "2");
 
     const std::string& c = parser.Get("c", NULL);
+// cout << "c='" << c << "'" << endl;
     assert(c == "");
 }
 
@@ -32,7 +38,7 @@ void test2()
     const std::string& a = parser.Get("a", NULL);
     assert(a == "1");
 
-    std::string b = parser.Get("a", NULL);
+    std::string b = parser.Get("b", NULL);
     assert(b == "2");
 
     const std::string& c = parser.Get("c", NULL);
@@ -50,11 +56,23 @@ void test3()
     const std::string& a = parser.Get("a", NULL);
     assert(a == "1");
 
-    std::string b = parser.Get("a", NULL);
+    std::string b = parser.Get("b", NULL);
     assert(b == "2");
 
     const std::string& c = parser.Get("c", NULL);
     assert(c == "3");
+}
+
+void test4()
+{
+    const char* ini_text= "a:1";
+    qh::INIParser parser;
+    if (!parser.Parse(ini_text, strlen(ini_text), "", ":")) {
+        assert(false);
+    }
+
+    const std::string& a = parser.Get("a", NULL);
+    assert(a == "1");
 }
 
 int main(int argc, char* argv[])
@@ -64,6 +82,7 @@ int main(int argc, char* argv[])
     test1();
     test2();
     test3();
+	test4();
 
     return 0;
 }
